2feb2efe226682e72957fa1feef24bccddfc7e00
jowatson2011 <jowatson@protonmail.com>
2024-07-16 16:38:01 +0100

    Styled nav as tab buttons with hover effect

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HeaderContent from './components/HeaderContent.vue'
 </script>
+<script>
+export default {
+  data() {
+    return {
+      tabStyle:
+        "tab-button z-1 px-4 py-2 text-gray-600 border-t-8 border-l border-r-8 hover:border-r-[6px] hover:border-t-[6px] border-sky-500 rounded-tl-xl rounded-tr-lg bg-pink-100 mr-4 ml-1 ease-out hover:translate-y+1 shadow"
+    }
+  }
+}
+</script>
 
 <template>
   <header>
@@ -9,8 +19,17 @@ import HeaderContent from './components/HeaderContent.vue'
       <HeaderContent />
 
       <nav class="flex justify-center">
-        <RouterLink to="/projects" class="pr-36">Projects</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/projects" :class="tabStyle">Projects</RouterLink>
+        <RouterLink
+          to="/experience"
+          :class="tabStyle"
+          >Experience</RouterLink
+        >
+        <RouterLink
+          to="/contact"
+          :class="tabStyle"
+          >Contact</RouterLink
+        >
       </nav>
     </div>
   </header>
