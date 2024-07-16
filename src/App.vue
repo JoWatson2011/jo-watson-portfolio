fc92edc7fb00e0be63705b63e437c940ae231cb4
jowatson2011 <jowatson@protonmail.com>
2024-07-16 15:07:05 +0100

    Project initiallised with vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderContent from './components/HeaderContent.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HeaderContent />
+
+      <nav class="flex justify-center">
+        <RouterLink to="/projects" class="pr-36">Projects</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,27 @@
+<!-- <script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script> -->
+
+<template>
+  <div class="flex items-center justify-center pt-8 pb-3">
+    <img
+      alt="stardew-valley-avatar"
+      class="rounded-full mr-3"
+      src="@/assets/avatar.png"
+      width="125"
+      height="125"
+    />
+    <div>
+      <h1 class="text-6xl">Jo Watson</h1>
+      <h2 class="text-2xl">Junior Software Developer</h2>
+    </div>
+  </div>
+  <p class="text-center mr-56 ml-56 pb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi incidunt itaque vero accusamus beatae. Minima fugit eligendi id dolor dolore repudiandae consequatur voluptatibus commodi. Inventore harum ratione qui impedit temporibus!</p>
+</template>
+
+<style scoped></style>
