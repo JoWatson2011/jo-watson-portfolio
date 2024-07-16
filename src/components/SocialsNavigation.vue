e2a96d433f205102470d7dd5d7832fb17215b4ed
jowatson2011 <jowatson@protonmail.com>
2024-07-16 17:16:09 +0100

    Added component for top nav bar containig links to socials

---
+++ src/components/SocialsNavigation.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      icons: [
+        { iconName: 'co-github', url: 'https://github.com/JoWatson2011' },
+        { iconName: 'bi-linkedin', url: 'https://www.linkedin.com/in/jolwatson/' }
+      ],
+      buttonStyle: ''
+    }
+  }
+}
+</script>
+<template>
+  <nav class="flex justify-end">
+      <div v-for="icon in icons" :key="icon.iconName" class="rounded-full hover:bg-green-400 p-1 mt-1 mr-1 opacity-50">
+        <a :href="icon.url"><v-icon :name="icon.iconName" scale="1.5" /></a>
+      </div>
+</nav>
+</template>
