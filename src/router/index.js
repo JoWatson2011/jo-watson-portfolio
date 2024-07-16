d9e599cd26cff941408329cbfa4b3b874406c88c
jowatson2011 <jowatson@protonmail.com>
2024-07-16 15:32:20 +0100

    Add route for contact

--- src/router/index.js
+++ src/router/index.js
@@ -10,15 +10,20 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/projects',
+      name: 'projects',
+
+      component: () => import('../views/ProjectsView.vue')
+    },
+    {
       path: '/experience',
       name: 'experience',
       component: () => import('../views/ExperienceView.vue')
     },
     {
-      path: '/projects',
-      name: 'projects',
-
-      component: () => import('../views/ProjectsView.vue')
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue')
     }
   ]
 })
