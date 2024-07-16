3f985b9c4d680937e91cf9221bdfc62888c49a81
jowatson2011 <jowatson@protonmail.com>
2024-07-16 16:50:05 +0100

    Import icons for use SocialsNavigation component

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { CoGithub, BiLinkedin } from 'oh-vue-icons/icons'
 import './assets/tailwind.css'
+
+addIcons(CoGithub, BiLinkedin)
 
 const app = createApp(App)
 
 app.use(router)
+app.component('v-icon', OhVueIcon)
 
 app.mount('#app')
