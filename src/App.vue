f02f582bea948ef5af2d7d2eb65e2833d7eb26bd
jowatson2011 <jowatson@protonmail.com>
2024-07-17 10:19:32 +0100

    Extract tab buttons into separate component

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,18 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HeaderContent from './components/HeaderContent.vue'
 import SocialsNavigation from './components/SocialsNavigation.vue'
-</script>
-<script>
-export default {
-  data() {
-    return {
-      tabStyle:
-        'tab-button z-1 px-4 py-2 text-gray-600 border-t-8 border-l border-r-8 hover:border-r-[6px] hover:border-t-[6px] border-sky-500 rounded-tl-xl rounded-tr-lg bg-pink-100 mr-4 ml-1 ease-out hover:translate-y+1 shadow'
-    }
-  }
-}
+import TabButtonNavigation from './components/TabButtonNavigation.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <SocialsNavigation/>
+      <SocialsNavigation />
       <HeaderContent />
-
-      <nav class="flex justify-center">
-        <RouterLink to="/projects" :class="tabStyle">Projects</RouterLink>
-        <RouterLink to="/experience" :class="tabStyle">Experience</RouterLink>
-        <RouterLink to="/contact" :class="tabStyle">Contact</RouterLink>
-      </nav>
     </div>
   </header>
 
-  <RouterView />
+  <TabButtonNavigation />
 </template>
 
 <style scoped></style>
---
+++ src/components/TabButtonNavigation.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      tabStyle:
+        'tab-button z-1 px-4 py-2 text-gray-600 border-t-8 border-l border-r-8 hover:border-r-[6px] hover:border-t-[6px] border-sky-500 rounded-tl-xl rounded-tr-lg bg-pink-100 mr-4 ml-1 ease-out hover:translate-y+1 shadow'
+    }
+  }
+}
+</script>
+
+<template>
+  <nav class="flex justify-center">
+    <RouterLink to="/projects" :class="tabStyle">Projects</RouterLink>
+    <RouterLink to="/experience" :class="tabStyle">Experience</RouterLink>
+    <RouterLink to="/contact" :class="tabStyle">Contact</RouterLink>
+  </nav>
+  <RouterView />
+</template>
