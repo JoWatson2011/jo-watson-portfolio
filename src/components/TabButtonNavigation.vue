bc0c5c6c10899b7481a867ce2db683d486961c43
jowatson2011 <jowatson@protonmail.com>
2024-07-17 11:52:57 +0100

    Fix max and min width constraints

--- src/components/TabButtonNavigation.vue
+++ src/components/TabButtonNavigation.vue
@@ -3,14 +3,14 @@ export default {
   data() {
     return {
       tabStyle:
-        'tab-button z-10 text-sm px-3 text-gray-600 border-t-8 border-l border-r-8 hover:border-r-[6px] hover:border-t-[6px] ease-out hover:translate-y+1 hover:translate-pb+3 border-sky-500 rounded-tl-xl rounded-tr-lg bg-pink-100 m-auto max-w-[375px] '
+        'tab-button mr-1 z-10 text-sm px-3 text-gray-600 border-t-8 border-l border-r-8 hover:border-r-[6px] hover:border-t-[6px] ease-out hover:translate-y+1 hover:translate-pb+3 border-sky-500 rounded-tl-xl rounded-tr-lg bg-pink-100 '
     }
   }
 }
 </script>
 
 <template>
-  <div class="relative">
+  <div class="relative flex flex-col justify-center place-items-center m-6">
     <RouterView />
     <nav class="flex justify-center items-stretch">
       <RouterLink to="/" :class="tabStyle">About</RouterLink>
--- src/views/general-styling.js
+++ src/views/general-styling.js
@@ -1,2 +1,2 @@
 export const containerStyle =
-  'absolute z-0 top-[21px] px-4 py-2 text-gray-600 border-t-8 border-l border-r-8 border-sky-500 round-tr-md bg-pink-100 m-auto min-w-full '
+  'absolute z-0 top-[20px] px-4 py-2 text-gray-600 border-t-8 border-l border-r-8 border-sky-500 rounded-tr-md rounded-b-lg bg-pink-100 min-w-[370px] max-w-[750px]'
