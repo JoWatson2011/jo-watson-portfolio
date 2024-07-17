e2afd89cae52cc231c11b3003ef7a2ee027ff225
jowatson2011 <jowatson@protonmail.com>
2024-07-17 16:47:12 +0100

    Change opacity of icons on hover

--- src/components/SocialsNavigation.vue
+++ src/components/SocialsNavigation.vue
@@ -16,7 +16,7 @@ export default {
     <div
       v-for="icon in icons"
       :key="icon.iconName"
-      class="rounded-full hover:bg-green-400 p-1 mt-1 mr-1 opacity-50"
+      class="rounded-full hover:opacity-100 p-1 mt-1 mr-1 opacity-50"
     >
       <a :href="icon.url"><v-icon :name="icon.iconName" scale="1.5" /></a>
     </div>
