07b543e59e5cb1d99bc6da484e45b1386ac91e09
jowatson2011 <jowatson@protonmail.com>
2024-07-17 11:52:39 +0100

    Moved about me text into HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,6 +14,15 @@ export default {
 
 <template>
   <main :class="containerStyle">
-    <h1>This is the home page</h1>
+    <p class="text-justify text-sm leading-tight p-8">
+      I am a scientist turned software developer. I spent the first part of my career as an academic
+      researcher analysing biological datasets using python (pandas, numpy, matplotlib) and R
+      (tidyverse). The most rewarding part of my scientific work was building my own pieces of
+      software to automate my data analysis and sharing these tools with my colleagues. This led me
+      to change my career and pursue software development. I have recently completed the Northcoders
+      software development bootcamp which gave me strong skills in JavaScript programming and
+      industry-relevent tech. As a former scientist, I am curious, analytical and always pushing
+      myself to learn new things, all of which feed into my work as a software developer.
+    </p>
   </main>
 </template>
