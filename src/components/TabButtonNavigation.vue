49241e2cd63ee5ae5e259daf37ef4d7c2258b7ba
jowatson2011 <jowatson@protonmail.com>
2024-07-18 08:53:18 +0100

    Add transitions between navigations

--- src/components/TabButtonNavigation.vue
+++ src/components/TabButtonNavigation.vue
@@ -11,7 +11,9 @@ export default {
 
 <template>
   <div class="relative flex flex-col justify-center place-items-center m-6">
-    <RouterView />
+    <transition name="page-turn" mode="out-in">
+      <RouterView />
+    </transition>
     <nav class="flex justify-center items-stretch">
       <RouterLink to="/" :class="tabStyle">About</RouterLink>
       <RouterLink to="/projects" :class="tabStyle">Projects</RouterLink>
@@ -20,3 +22,22 @@ export default {
     </nav>
   </div>
 </template>
+
+<style>
+.page-turn-enter-active,
+.page-turn-leave-active {
+  @apply transition-opacity duration-500 ease-in-out;
+}
+.page-turn-enter-from {
+  @apply transform translate-x-full opacity-0;
+}
+.page-turn-enter-to {
+  @apply transform translate-x-0 opacity-100;
+}
+.page-turn-leave-from {
+  @apply transform translate-x-0 opacity-100;
+}
+.page-turn-leave-to {
+  @apply transform -translate-x-full opacity-0;
+}
+</style>
