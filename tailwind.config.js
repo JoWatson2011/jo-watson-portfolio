5daf51a2f3611e2491586d84b1ecb06d8ecd7f1c
jowatson2011 <jowatson@protonmail.com>
2024-07-18 08:37:35 +0100

    Added custom shadow styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        'block-page': `
+        2px -5px 2px 1px rgb(96, 207, 247),
+        3px -6px 2px 1px rgb(132, 248, 255)
+        `,
+        'block-tab': `
+        1px -5px 2px 1px rgb(96, 207, 247),
+        1px -6px 2px 1px rgb(132, 248, 255)
+        `
+      }
+    }
   },
   plugins: []
 }
