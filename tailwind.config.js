ca2ca0bbe204ad032261206176a97153c64d268e
jowatson2011 <jowatson@protonmail.com>
2024-07-18 10:15:44 +0100

    Add custom shadow for project cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         'block-tab': `
         1px -5px 2px 1px rgb(96, 207, 247),
         1px -6px 2px 1px rgb(132, 248, 255)
+        `,
+        'project-card': `
+        2px -2px 5px 3px white
         `
       }
     }
