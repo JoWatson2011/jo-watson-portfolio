7c2760fba0b783e3ae2ff85092c69aca53ab186e
jowatson2011 <jowatson@protonmail.com>
2024-07-19 11:10:14 +0100

    Added navigation as a list to use instead of tab buttons

---
+++ src/components/NavList.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+        linkStyle: ""
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="">
+    <nav class="flex flex-col justify-center items-stretch">
+      <RouterLink to="/projects" :class="linkStyle">Projects</RouterLink>
+      <RouterLink to="/experience" :class="linkStyle">Experience</RouterLink>
+      <RouterLink to="/contact" :class="linkStyle">Contact</RouterLink>
+    </nav>
+  </div>
+</template>
