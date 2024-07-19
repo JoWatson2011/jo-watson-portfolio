aae35337e6d98f4ab07a7fd291e9411a0ce169b0
jowatson2011 <jowatson@protonmail.com>
2024-07-19 16:58:27 +0100

    Changed styling, made border thicker

--- src/components/PostIt.vue
+++ src/components/PostIt.vue
@@ -1,11 +1,11 @@
 <template>
   <section
-    class="relative  border border-amber-950 rounded rounded-br-[30px] bg-yellow-100 shadow-project-card"
+    class="relative  border-2 border-amber-950 rounded rounded-br-[30px] bg-yellow-100 shadow-project-card"
   >
     <div class="relative h-[30px] w-full bg-yellow-200 rounded-t-lg mb-2">&nbsp;</div>
     <slot></slot>
     <div
-      class="absolute bottom-0 right-0 w-[30px] h-[30px] bg-yellow-200 rounded-br-[30px] border-t border-l border-amber-950"
+      class="absolute bottom-0 right-0 w-[30px] h-[30px] bg-yellow-200 rounded-br-[30px] border-t-2 border-l-2 border-amber-950"
     ></div>
   </section>
 </template>
