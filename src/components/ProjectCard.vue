016052ca3fa4545049e9761de0265d42af27390e
jowatson2011 <jowatson@protonmail.com>
2024-07-19 12:08:28 +0100

    Created project card with basic styling

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import PostIt from './PostIt.vue'
+</script>
+<script>
+export default {
+  props: {
+    Title: String,
+    Image: String,
+    description: String,
+    frontEndURL: String,
+    backEndURL: String,
+    hostedURL: String,
+    techStackIcons: Object
+  }
+}
+</script>
+
+<template>
+  <PostIt>
+    <p>{{ Title }}</p>
+    <p>{{ description }}</p>
+  </PostIt>
+</template>
