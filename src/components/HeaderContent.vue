9b4f8725248e7c4ecb5c63666be222d96327b6d8
jowatson2011 <jowatson@protonmail.com>
2024-07-19 12:05:47 +0100

    Wrapper image and h1 in RouterLink to root

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="flex flex-col items-center justify-center pt-8 pb-8">
-    <img
-      alt="stardew-valley-avatar"
-      class="rounded-full mr-[10px]"
-      src="@/assets/avatar.png"
-      width="125"
-      height="125"
-    />
+    <RouterLink to="/">
+      <img
+        alt="stardew-valley-avatar"
+        class="rounded-full mr-[10px]"
+        src="@/assets/avatar.png"
+        width="125"
+        height="125"
+      />
+    </RouterLink>
     <div>
-      <h1 class="text-[2rem] font-semibold whitespace-nowrap text-center">Jo Watson</h1>
+      <RouterLink to="/">
+        <h1 class="text-[2rem] font-semibold whitespace-nowrap text-center">Jo Watson</h1>
+      </RouterLink>
       <h2 class="text-[1.5rem] whitespace-nowrap text=center">Junior Software Developer</h2>
     </div>
   </div>
