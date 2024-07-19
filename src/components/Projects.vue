b36d63640e2656869371e2da1337fd06c862969d
jowatson2011 <jowatson@protonmail.com>
2024-07-19 12:07:19 +0100

    Added dropdown and event handler for select that updates the details sent to project card on props

---
+++ src/components/Projects.vue
@@ -0,0 +1,44 @@
+<script setup>
+import projects from '../assets/projects.js'
+import ProjectCard from './ProjectCard.vue'
+</script>
+
+<script>
+export default {
+  components: {
+    ProjectCard
+  },
+  data() {
+    return {
+      projects: projects,
+      selected: 'NC news',
+      projectDetails: {}
+    }
+  },
+  methods: {
+    onSelect() {
+      console.log(this.selected)
+      this.projectDetails = this.projects.filter((project) => project.Title === this.selected)[0]
+    }
+  },
+  mounted() {
+    this.onSelect()
+  }
+}
+</script>
+
+<template>
+  <div class="flex justify-center my-4">
+    <label for="project-select" class="mr-5">Select a project: </label>
+    <select
+      name="project"
+      id="project-select"
+      v-model="selected"
+      @change="onSelect"
+      class="rounded-full border-sky-800 border-2"
+    >
+      <option v-for="project in projects" :value="project.Title">{{ project.Title }}</option>
+    </select>
+  </div>
+  <ProjectCard :Title="projectDetails.Title" :description="projectDetails.description" />
+</template>
