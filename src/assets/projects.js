d60e0f53505e849e89dd11af8b2287e6522074d4
jowatson2011 <jowatson@protonmail.com>
2024-07-19 12:05:09 +0100

    Added data for project cards

---
+++ src/assets/projects.js
@@ -0,0 +1,55 @@
+const projects = [
+  {
+    Title: 'RumbleQuiz',
+    imageLocation: '',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et.',
+    frontEndURL: 'https://github.com/JoWatson2011/rumble-quiz-app',
+    backEndURL: 'https://github.com/JoWatson2011/rumble-quiz-server',
+    hostedURL: '',
+    techStackIcons: [
+      { name: 'vi-file-type-js-official', label: 'JavaScript' },
+      { name: 'si-express', label: 'express' },
+      { name: 'vi-file-type-pgsql', label: 'PSQL' },
+      { name: 'co-socket-io', label: 'SocketIo' },
+      { name: 'vi-file-type-react-js', label: 'ReactNative' }
+    ]
+  },
+  {
+    Title: 'NC news',
+    imageLocation: '',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et.',
+    frontEndURL: 'https://github.com/JoWatson2011/fe-nc-news',
+    backEndURL: 'https://github.com/JoWatson2011/nc-news',
+    hostedURL: '',
+    techStackIcons: [
+      { name: 'vi-file-type-js-official', label: 'JavaScript' },
+      { name: 'si-express', label: 'express' },
+      { name: 'vi-file-type-pgsql', label: 'PSQL' },
+      { name: 'vi-file-type-react-js', label: 'React' },
+      { name: 'vi-file-type-tailwind', label: 'Tailwindcss' }
+    ]
+  },
+  {
+    Title: 'Pokemon CLI Battler',
+    imageLocation: '',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et.',
+    backEndURL: 'https://github.com/JoWatson2011/pokemon-battler/tree/main',
+    frontEndURL: null,
+    hostedURL: '',
+    techStackIcons: [{ name: 'vi-file-type-js-official', label: 'JavaScript' }]
+  },
+  {
+    Title: 'SubcellulaRVis',
+    imageLocation: '',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et.',
+    frontEndURL: 'https://github.com/JoWatson2011/subcellularvis',
+    backEndURL: null,
+    hostedURL: 'phenome.manchester.ac.uk/subcellular',
+    techStackIcons: [{ name: 'vi-file-type-rproj', label: 'R' }]
+  }
+]
+export default projects
