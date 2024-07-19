54077eb9b3e80af8c151db47b6788bc958bbc9df
jowatson2011 <jowatson@protonmail.com>
2024-07-19 11:01:38 +0100

    Update text and styling in bio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,3 @@
-<script setup>
-import { containerStyle } from './general-styling'
-</script>
-
 <script>
 export default {
   data() {
@@ -11,37 +7,30 @@ export default {
         'vi-file-type-python',
         'vi-file-type-pgsql',
         'vi-file-type-rproj'
-      ],
-      containerStyle
+      ]
     }
   }
 }
 </script>
 
 <template>
-  <main :class="containerStyle">
-    <h3 class="px-8 pt-4 font-bold">Tech Stack</h3>
-    <div class="flex justify-center p-2">
-      <v-icon v-for="icon in icons" :name="icon" scale="2.5" />
-    </div>
-
-    <h3 class="px-8 pt-4 font-bold">Bio</h3>
+  <main>
+      <div class="flex justify-center p-2">
+        <v-icon v-for="icon in icons" :name="icon" scale="2.5" />
+      </div>
+    <br />
     <p class="text-justify text-sm leading-tight px-8">
-      I am a scientist turned software developer. My scientific background means I am curious,
-      analytical and always pushing myself to learn new things, all of which feed into my work as a
-      software developer.
+      <span class="font-semibold">
+        I am a scientist turned software developer. My scientific background means I am curious,
+        analytical and always pushing myself to learn new things, all of which feed into my work as
+        a developer.
+      </span>
       <br />
       <br />
-      I spent the first part of my career as an academic researcher analysing biological datasets
-      using python (pandas, numpy, matplotlib) and R (tidyverse). The most rewarding part of my
-      scientific work was building my own pieces of software to automate my data analysis and
-      sharing these tools with my colleagues. This led me to change my career and pursue software
-      development.
-      <br />
-      <br />
-      I have recently completed the Northcoders software development bootcamp which gave me strong
-      skills in JavaScript programming for front-end and back-end development, as well as
-      methodolgies including TDD, paired programming, Agile/SCRUM.
+      The most rewarding part of my scientific work was building my own pieces of software to
+      automate my data analysis and sharing these tools with my colleagues. This led me to change my
+      career and pursue software development. I have recently completed the Northcoders software
+      development bootcamp and I am now actively looking for my first role in tech
     </p>
   </main>
 </template>
