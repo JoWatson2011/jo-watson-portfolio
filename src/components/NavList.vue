50275c6711d2fb518a1e5bdc2e0472ba9df14bfa
jowatson2011 <jowatson@protonmail.com>
2024-07-19 17:24:53 +0100

    Styled RouterLinks and added icons

--- src/components/NavList.vue
+++ src/components/NavList.vue
@@ -1,19 +1,38 @@
-<script>
-export default {
-  data() {
-    return {
-        linkStyle: ""
-    }
-  }
-}
+<script setup>
+import { BiArchiveFill, IoDocumentText, BiEnvelopeOpenFill } from 'oh-vue-icons/icons'
 </script>
 
 <template>
   <div class="">
     <nav class="flex flex-col justify-center items-stretch">
-      <RouterLink to="/projects" :class="linkStyle">Projects</RouterLink>
-      <RouterLink to="/experience" :class="linkStyle">Experience</RouterLink>
-      <RouterLink to="/contact" :class="linkStyle">Contact</RouterLink>
+      <RouterLink
+        to="/projects"
+        class="hover:text-light-midBlue text-lg"
+        activeClass="font-extrabold text-light-darkBlue"
+        ><v-icon
+          name="bi-archive-fill"
+          scale="1.25"
+        />
+        Projects</RouterLink
+      >
+      <RouterLink
+        to="/experience"
+        class="hover:text-light-midBlue text-lg"
+        activeClass="font-extrabold text-light-darkBlue"
+        ><v-icon
+          name="io-document-text"
+          scale="1.25"
+        />Experience</RouterLink
+      >
+      <RouterLink
+        to="/contact"
+        class="hover:text-light-midBlue text-lg"
+        activeClass="font-extrabold text-light-darkBlue"
+        ><v-icon
+          name="bi-envelope-open-fill"
+          scale="1.25"
+        />Contact</RouterLink
+      >
     </nav>
   </div>
 </template>
