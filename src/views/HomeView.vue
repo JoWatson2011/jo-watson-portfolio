787774db49f1e20653376285208f4bc8eeaa97a2
jowatson2011 <jowatson@protonmail.com>
2024-07-22 09:42:43 +0100

    Add header to bio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,19 +22,21 @@ export default {
       <v-icon v-for="icon in icons" :name="icon" scale="2.5" />
     </div>
     <br />
-    <PostIt class="mx-10">
-      <p class="text-justify text-sm leading-tight p-8">
-        <span class="font-semibold">
-          I am a scientist turned software developer. My scientific background means I am curious,
-          analytical and always pushing myself to learn new things, all of which feed into my work
-          as a developer.
-        </span>
-        <br />
-        The most rewarding part of my scientific work was building my own pieces of software to
-        automate my data analysis and sharing these tools with my colleagues. This led me to change
-        my career and pursue software development. I have recently completed the Northcoders
-        software development bootcamp and I am now actively looking for my first role in tech
-      </p>
-    </PostIt>
+      <PostIt class="mx-10"d>
+        <h3 class="text-xl px-8 font-bold">Welcome!</h3>
+        <p class="text-justify text-sm leading-tight px-8 pb-4">
+          <span class="font-semibold">
+            I am a scientist turned software developer. My scientific background means I am curious,
+            analytical and always pushing myself to learn new things, all of which feed into my work
+            as a developer.
+          </span>
+          <br />
+          The most rewarding part of my scientific work was building my own pieces of software to
+          automate my data analysis and sharing these tools with my colleagues. This led me to
+          change my career and pursue software development. I have recently completed the
+          Northcoders software development bootcamp and I am now actively looking for my first role
+          in tech
+        </p>
+      </PostIt>
   </main>
 </template>
