bfaf4486c3a53f0925291e0fcd4e74e45dff6959
jowatson2011 <jowatson@protonmail.com>
2024-07-22 16:03:59 +0100

    Created reusable component for displaying work experience

---
+++ src/components/ExperienceItem.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: ['details']
+}
+</script>
+
+<template>
+  <section class="flex items-center my-2 bg-light-lightBlue/20 rounded-lg p-1 ">
+    <div class="border border-white rounded-full bg-white mx-3">
+      <v-icon :name="details.iconName" scale="2" :class="details.iconColour" />
+    </div>
+    <div class="flex flex-col">
+      <div class="flex flex-wrap justify-between">
+        <h3 class="font-bold text-[16px]">
+          {{ details.heading }}
+        </h3>
+        <p class="text-sm">
+          {{ details.dates }}
+        </p>
+      </div>
+      <p>
+        {{ details.subtitle }}
+      </p>
+      <p class="text-sm italic">
+        {{ details.description }}
+      </p>
+    </div>
+  </section>
+</template>
