a3ec2075f19cf0b8fa33e88893891892f7890312
jowatson2011 <jowatson@protonmail.com>
2024-07-22 16:05:46 +0100

    Added animation on hover

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -3,7 +3,7 @@
     <RouterLink to="/">
       <img
         alt="stardew-valley-avatar"
-        class="rounded-full mr-[10px] shadow-block-image"
+        class="rounded-full mr-[10px] shadow-block-image hover:-translate-y-px hover:translate-x-px transition-all hover:shadow-block-image-hover"
         src="@/assets/avatar.png"
         width="125"
         height="125"
