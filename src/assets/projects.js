df88b8b7a13867c87ad26e77374fbf136da94678
jowatson2011 <jowatson@protonmail.com>
2024-07-22 16:42:27 +0100

    Edited descriptions to ensure they fit on two lines when rendered

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -4,7 +4,7 @@ const projects = [
     imageLocation:
       'https://github.com/JoWatson2011/portfolio-website/blob/main/src/assets/project-card-images/rumble-quiz.jpeg?raw=true',
     description:
-      'A mobile, multiplayer trivia game',
+      'A multiplayer trivia game built for mobile. Select a topic and play head-to-head against other players.',
     frontEndURL: 'https://github.com/JoWatson2011/rumble-quiz-app',
     backEndURL: 'https://github.com/JoWatson2011/rumble-quiz-server',
     hostedURL: '',
@@ -20,8 +20,7 @@ const projects = [
     Title: 'NC news',
     imageLocation:
       'https://github.com/JoWatson2011/portfolio-website/blob/main/src/assets/project-card-images/nc-news.png?raw=true',
-    description:
-      'A social news app where users can post, comment and vote on articles.',
+    description: 'A social news app where users can post, comment and vote on articles.',
     frontEndURL: 'https://github.com/JoWatson2011/fe-nc-news',
     backEndURL: 'https://github.com/JoWatson2011/nc-news',
     hostedURL: 'https://jowatson-portfolio-nc-news.netlify.app',
@@ -37,8 +36,7 @@ const projects = [
     Title: 'Pokemon CLI Battler',
     imageLocation:
       'https://github.com/JoWatson2011/portfolio-website/blob/main/src/assets/project-card-images/pokemon-battler-terminal.png?raw=true',
-    description:
-      'An interactive command-line game based on a Pokemon battle.',
+    description: 'An interactive command-line game based on a Pokemon battle.',
     backEndURL: 'https://github.com/JoWatson2011/pokemon-battler/tree/main',
     frontEndURL: null,
     hostedURL: null,
@@ -49,7 +47,7 @@ const projects = [
     imageLocation:
       'https://github.com/JoWatson2011/portfolio-website/blob/main/src/assets/project-card-images/subcellularvis.png?raw=true',
     description:
-      'A web app to facilitate analysis and understanding of protein localisation from high-throughput biological experiments.',
+      'A web app to facilitate analysis of data from high-throughput biological experiments.',
     frontEndURL: 'https://github.com/JoWatson2011/subcellularvis',
     backEndURL: null,
     hostedURL: 'phenome.manchester.ac.uk/subcellular',
