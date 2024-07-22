621c2da32f3fc57586ee08db92b0483f3cd6b587
jowatson2011 <jowatson@protonmail.com>
2024-07-22 16:43:36 +0100

    Added data for experience page

---
+++ src/assets/experience.js
@@ -0,0 +1,39 @@
+const experience = [
+  {
+    heading: 'Northcoders',
+    iconName: 'ri-arrow-up-s-line',
+    iconColour: 'text-red-800',
+    dates: 'April 2024 - Jun 2024',
+    subtitle: 'Software Development Bootcamp',
+    description:
+      'Intensive 13-week coding bootcamp teaching full-stack software development based on industry standards and best practices.'
+  },
+  {
+    heading: 'University of Manchester',
+    iconName: 'ri-test-tube-line',
+    iconColour: 'text-green-700',
+    dates: 'Oct 2021 - Mar 2024',
+    subtitle: 'Research Scientist',
+    description:
+      'Modelling and analaysis of biological datasets with Python and R, to understand biopharmaceutical processes. Teaching undergraduate students programming skills in python.'
+  },
+  {
+    heading: 'University of Manchester',
+    iconName: 'la-university-solid',
+    iconColour: 'text-purple-900',
+    dates: 'Sept 2017 - Sept 2021',
+    subtitle: 'PhD in Bioinformatics',
+    description:
+      'Modelling and analaysis of biological datasets with Python and R, to understand how cancer cells respond to changes in their environment. First author on three peer reviewed papers.'
+  },
+  {
+    heading: 'University of Manchester',
+    iconName: 'la-university-solid',
+    iconColour: 'text-yellow-600',
+    dates: 'Sept 2012 - Sept 2016',
+    subtitle: 'BSc Cell Biology',
+    description: 'First Class'
+  }
+]
+
+export default experience
