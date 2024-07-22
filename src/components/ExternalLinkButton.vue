af01cf19dc82e358a7ff3e14cfcdc3197ee9f93a
jowatson2011 <jowatson@protonmail.com>
2024-07-22 12:46:49 +0100

    Created button component for external links on project cards

---
+++ src/components/ExternalLinkButton.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    iconName: String,
+    url: String,
+    text: String
+  }
+}
+</script>
+
+<template>
+  <button
+    :name="text"
+    class="bg-white rounded-full px-2 shadow-block-dropdown m-2 ease-out hover:shadow-button-hover hover:-translate-y-px hover:translate-x-px transition-all"
+  >
+    <a :href="url">
+      <v-icon :name="iconName" />
+      {{ text }}
+    </a>
+  </button>
+</template>
