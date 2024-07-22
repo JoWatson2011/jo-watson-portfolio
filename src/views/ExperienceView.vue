2e6091e985683db931b89f664911d63e57e0f285
jowatson2011 <jowatson@protonmail.com>
2024-07-22 16:09:06 +0100

    Styled ExperienceView with scrollable element if appSize is large

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -4,16 +4,39 @@ import experience from '../assets/experience.js'
 </script>
 <script>
 export default {
+  inject: ['windowSize'],
   data() {
     return {
-      experience
+      appSize: this.windowSize,
+      experience,
+      scrollContainer: ''
+    }
+  },
+  methods: {
+    setScrollContainer() {
+      this.scrollContainer =
+        this.appSize === 'large'
+          ? 'overflow-scroll scroll-smooth bg-light-lightBlue/10 ml-5 my-5 p-2 border rounded-lg border-gray-900'
+          : ''
+    }
+  },
+  mounted() {
+    this.setScrollContainer()
+  },
+  watch: {
+    appSize(size) {
+      this.setScrollContainer()
     }
   }
 }
 </script>
 
 <template>
+  <div
+    :class="scrollContainer"
+  >
     <main class="justify-self-start">
       <ExperienceItem v-for="item in experience" :details="item" />
     </main>
+  </div>
 </template>
