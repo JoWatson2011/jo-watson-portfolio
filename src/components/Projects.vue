d7be93647cc9528a1deef9fc80a25403c771f54d
jowatson2011 <jowatson@protonmail.com>
2024-07-22 10:29:41 +0100

    Added styling to dropdown and passed down data on props to ProjectCard

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -29,16 +29,24 @@ export default {
 
 <template>
   <div class="flex justify-center my-4">
-    <label for="project-select" class="mr-5">Select a project: </label>
+    <label for="project-select" class="mr-5 text-black font-medium">Select a project: </label>
     <select
       name="project"
       id="project-select"
       v-model="selected"
       @change="onSelect"
-      class="rounded-full border-sky-800 border-2"
+      class="rounded-full shadow-block-dropdown"
     >
       <option v-for="project in projects" :value="project.Title">{{ project.Title }}</option>
     </select>
   </div>
-  <ProjectCard :Title="projectDetails.Title" :description="projectDetails.description" />
+  <ProjectCard
+    :Title="projectDetails.Title"
+    :description="projectDetails.description"
+    :imageLocation="projectDetails.imageLocation"
+    :frontEndURL="projectDetails.frontEndURL"
+    :backEndURL="projectDetails.backEndURL"
+    :hostedURL="projectDetails.hostedURL"
+    :techStackIcons="projectDetails.techStackIcons"
+  />
 </template>
