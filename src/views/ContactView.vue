69e15c1e4027a8da5f92a09f7dbc0ffed3aa4689
jowatson2011 <jowatson@protonmail.com>
2024-07-23 10:25:37 +0100

    Implemeneted emailjs to handle sending email on form submission

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,18 +1,36 @@
 <script setup>
 import ExternalLinkButton from '../components/ExternalLinkButton.vue'
+import emailjs from '@emailjs/browser'
 </script>
 
 <script>
 export default {
   data() {
     return {
-      name: '',
-      email: '',
+      from_name: '',
+      from_email: '',
       message: ''
     }
   },
   methods: {
-    submitForm() {}
+    submitForm() {
+      emailjs
+        .sendForm(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          this.$refs.contactForm,
+          {
+            publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+          }
+        )
+        .then(() => {
+          console.log('Thank you for your email!')
+        })
+        .catch((error) => {
+          console.log('Error sending email', error)
+          alert('Failed to send message. Please try again later.')
+        })
+    }
   }
 }
 </script>
@@ -20,26 +38,29 @@ export default {
 <template>
   <div class="my-8 mx-10">
     <p class="text-black pb-3 text-center">
-      I'd love to hear from you if you have any feedback on this website or any opportunities available!
+      I'd love to hear from you if you have any feedback on this website or any opportunities
+      available!
     </p>
-    <form @submit.prevent="submitForm" class = " flex flex-col justify-items-center">
+    <form ref="contactForm" @submit.prevent="submitForm" class="flex flex-col justify-items-center">
       <div class="mb-4">
-        <label for="name" class="text-sm font-medium text-gray-700">Name</label>
+        <label for="from_name" class="text-sm font-medium text-gray-700">Name</label>
         <input
           type="text"
-          v-model="name"
-          id="name"
-          class="mt-1 p-2  w-full border border-gray-300 rounded-md"
+          v-model="from_name"
+          id="from_name"
+          name="from_name"
+          class="mt-1 p-2 w-full border border-gray-300 rounded-md"
           required
         />
       </div>
       <div class="mb-4">
-        <label for="email" class=" text-sm font-medium text-gray-700">Email</label>
+        <label for="from_email" class="text-sm font-medium text-gray-700">Email</label>
         <input
           type="email"
-          v-model="email"
-          id="email"
-          class="mt-1 p-2  w-full border border-gray-300 rounded-md"
+          v-model="from_email"
+          id="from_email"
+          name="from_email"
+          class="mt-1 p-2 w-full border border-gray-300 rounded-md"
           required
         />
       </div>
@@ -48,6 +69,7 @@ export default {
         <textarea
           v-model="message"
           id="message"
+          name="message"
           class="mt-1 p-2 w-full border border-gray-300 rounded-md"
           rows="3"
           required
@@ -55,7 +77,7 @@ export default {
       </div>
       <button
         type="submit"
-        class=" bg-blue-500 text-white rounded-full px-2 shadow-block-dropdown ease-out hover:shadow-button-hover hover:-translate-y-px hover:translate-x-px transition-all"
+        class="bg-blue-500 text-white rounded-full px-2 shadow-block-dropdown ease-out hover:shadow-button-hover hover:-translate-y-px hover:translate-x-px transition-all"
       >
         Send
       </button>
