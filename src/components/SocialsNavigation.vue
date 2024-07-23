5a1729b36f059b0069b0dfc1a40c113ce1ecab9c
jowatson2011 <jowatson@protonmail.com>
2024-07-23 14:47:48 +0100

    removed comments

--- src/components/SocialsNavigation.vue
+++ src/components/SocialsNavigation.vue
@@ -44,12 +44,5 @@ export default {
       :url="icon.url"
       :text="icon.text"
     />
-    <!-- <div
-      v-for="icon in icons"
-      :key="icon.iconName"
-      class="rounded-full hover:opacity-100 p-1 mt-1 mr-1 opacity-50"
-    >
-      <a :href="icon.url"><v-icon :name="icon.iconName" scale="1.5" /></a>
-    </div> -->
   </nav>
 </template>
