9064a3329de6e33607b89f03a4c68eb0040dbbe8
jowatson2011 <jowatson@protonmail.com>
2024-07-23 14:02:24 +0100

    WAVE: Wrapped all content in a single RouterLink rather than have adjacent RouterLinks to the same place

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col items-center justify-center pt-8 pb-4">
-    <RouterLink to="/">
+  <RouterLink to="/">
+    <div class="flex flex-col items-center justify-center pt-8 pb-4">
       <img
         alt="stardew-valley-avatar"
         class="rounded-full mr-[10px] shadow-block-image hover:-translate-y-px hover:translate-x-px transition-all hover:shadow-block-image-hover"
@@ -8,18 +8,16 @@
         width="125"
         height="125"
       />
-    </RouterLink>
-    <div>
-      <RouterLink to="/">
+      <div>
         <h1 class="text-[2rem] font-semibold whitespace-nowrap text-center text-black">
           Jo Watson
         </h1>
-      </RouterLink>
-      <h2 class="text-[1.5rem] whitespace-nowrap text=center text-black">
-        Junior Software Developer
-      </h2>
+        <h2 class="text-[1.5rem] whitespace-nowrap text=center text-black">
+          Junior Software Developer
+        </h2>
+      </div>
     </div>
-  </div>
+  </RouterLink>
 </template>
 
 <style scoped></style>
