8a254d058cfe20cea1c20bc259cf124f37862532
jowatson2011 <jowatson@protonmail.com>
2024-07-24 10:01:42 +0100

    Abstracted biography content from HomeView

---
+++ src/components/Bio.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3 class="text-xl px-8 font-bold">Welcome!</h3>
+  <p class="text-justify text-sm leading-tight px-8 pb-4">
+    <span class="font-semibold">
+      I am a scientist turned software developer. My scientific background means I am curious,
+      analytical and always pushing myself to learn new things, all of which feed into my work as a
+      developer.
+    </span>
+    <br />
+    I have recently completed the Northcoders software development bootcamp and I am now actively
+    looking for my first role in tech. I have built front- and back-end projects using a range of
+    JavaScript frameworks and bring a strong background in data handling (including databases),
+    analysis and visualisation.
+  </p>
+</template>
