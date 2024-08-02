de240f1a6474f0ef470e3904551590e04f83a4dd
jowatson2011 <jowatson@protonmail.com>
2024-08-02 16:41:38 +0100

    Tested NavList links navigate to expected views and paths

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -6,12 +6,23 @@ describe('Navigation', () => {
     cy.get('[data-cy="nav-list"]').should('exist')
     cy.get('[data-cy="socials-navigation"]').should('exist')
   })
+  it('Navigates to root when the header is selected', () => {
+    cy.visit('/projects')
+
+    cy.get('header').click().get('[data-cy="home-view"]').should('exist')
+  })
   it('Renders the projects view on /projects', () => {
     cy.visit('/projects')
     cy.get('[data-cy="projects"]').should('exist')
     cy.get('header').should('exist')
     cy.get('[data-cy="nav-list"]').should('exist')
     cy.get('[data-cy="socials-navigation"]').should('exist')
+  })
+  it('Navigates to /projects when projects is selected from the NavList', () => {
+    cy.visit('/')
+    cy.get('[data-cy="projects-link"]').click()
+    cy.location('pathname').should('equal', '/projects')
+    cy.get('[data-cy="projects"]').should('exist')
   })
   it('Renders the experience view on /experience', () => {
     cy.visit('/experience')
@@ -20,11 +31,23 @@ describe('Navigation', () => {
     cy.get('[data-cy="nav-list"]').should('exist')
     cy.get('[data-cy="socials-navigation"]').should('exist')
   })
+  it('Navigates to /experience when Experience is selected from the NavList', () => {
+    cy.visit('/')
+    cy.get('[data-cy="experience-link"]').click()
+    cy.location('pathname').should('equal', '/experience')
+    cy.get('[data-cy="experience"]').should('exist')
+  })
   it('Renders the contact view on /contact', () => {
     cy.visit('/contact')
     cy.get('[data-cy="contact"]').should('exist')
     cy.get('header').should('exist')
     cy.get('[data-cy="nav-list"]').should('exist')
     cy.get('[data-cy="socials-navigation"]').should('exist')
+  })
+  it('Navigates to /contact when Contact is selected from the NavList', () => {
+    cy.visit('/')
+    cy.get('[data-cy="contact-link"]').click()
+    cy.location('pathname').should('equal', "/contact")
+    cy.get('[data-cy="contact"]').should('exist')
   })
 })
