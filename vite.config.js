5dfefc4c3358c08742f516c9538d5e1bdfdc693a
jowatson2011 <jowatson@protonmail.com>
2024-08-05 12:11:56 +0100

    Added vite-plugin-vercel to config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), vercel()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
